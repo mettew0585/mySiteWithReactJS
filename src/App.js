a76fcea1423072bfb96aa9e80e548cc392970538
JOJO <48712755+mettew0585@users.noreply.github.com>
2022-04-04 16:23:28 +0900

    first upload

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Button from "./Button";
+import styles from "./App.module.css";
+import myImage1 from "./img/1.jpg";
 
-function App() {
+function Bio() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        display: "flex",
+        margin: "1vw",
+      }}
+    >
+      <img src={myImage1}></img>
+      <div style={{ display: "flex", alignItems: "center" }}>
+        <ul>
+          <li>1999 : Born in Daegu, South Korea</li>
+          <li>`15~`18` : Gyeong sin HighSchool</li>
+          <li>
+            `18~ : KyungPook National University (major in Mobile enginering)
+          </li>
+          <li>`20~`22 : Air Force</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+function Contact() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        margin: "1vw",
+      }}
+    >
+      <img src={myImage1}></img>
+      <div style={{ display: "flex", alignItems: "center" }}>
+        <ul>
+          <li>E-mail : mettew0585@gmail.com</li>
+          <li>Instagram : lauveun</li>
+          <li>Github : mettew0585</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+function App() {
+  const [d, setD] = useState(false);
+  const onClick1 = (event) => {
+    setD(false);
+  };
+  const onClick2 = () => {
+    setD(true);
+  };
+  return (
+    <div>
+      <div
+        id="menu"
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          borderBottom: "0.2vw black solid",
+        }}
+      >
+        <h1>Cho Hyeon Woo</h1>
+        <ul style={{ display: "flex" }}>
+          <button onClick={onClick1}>Bio</button>
+          <button onClick={onClick2}>Contact</button>
+        </ul>
+      </div>
+      <div>{d ? <Contact></Contact> : <Bio></Bio>}</div>
     </div>
   );
 }
